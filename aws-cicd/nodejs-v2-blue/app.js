efea10c6fe5fe8ed01059079b176172591e2795d
omonuj <jonahodoh@gmail.com>
2023-02-08 22:43:54 +0100

    Grow Express server for the blue deployment

--- aws-cicd/nodejs-v2-blue/app.js
+++ aws-cicd/nodejs-v2-blue/app.js
@@ -18,3 +18,13 @@ var server = http.createServer(function (req, res) {
         req.on('end', function() {
             if (req.url === '/') {
                 log('Received message: ' + body);
+            } else if (req.url === '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
