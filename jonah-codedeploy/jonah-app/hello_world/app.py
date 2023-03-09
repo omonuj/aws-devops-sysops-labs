843dad2c96ecf782f4313b3642dac8a2e2f182b9
omonuj <jonahodoh@gmail.com>
2023-03-09 17:34:50 +0100

    Grow Lambda handler for the hello_world function

--- jonah-codedeploy/jonah-app/hello_world/app.py
+++ jonah-codedeploy/jonah-app/hello_world/app.py
@@ -9,3 +9,14 @@ def lambda_handler(event, context):
     Parameters
     ----------
     event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
