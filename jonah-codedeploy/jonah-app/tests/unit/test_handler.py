ed8c2a81be234e88f96ecaaf76292efe7e838cf9
omonuj <jonahodoh@gmail.com>
2023-03-27 11:06:26 +0100

    Finalize unit tests for the Lambda handler

--- jonah-codedeploy/jonah-app/tests/unit/test_handler.py
+++ jonah-codedeploy/jonah-app/tests/unit/test_handler.py
@@ -60,3 +60,14 @@ def apigw_event():
         "stageVariables": {"baz": "qux"},
         "path": "/examplepath",
     }
+
+
+def test_lambda_handler(apigw_event, mocker):
+
+    ret = app.lambda_handler(apigw_event, "")
+    data = json.loads(ret["body"])
+
+    assert ret["statusCode"] == 200
+    assert "message" in ret["body"]
+    assert data["message"] == "hello world"
+    # assert "location" in data.dict_keys()
