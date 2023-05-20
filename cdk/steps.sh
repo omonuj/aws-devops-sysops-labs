11247e37e889dbdc9f49dd65c94b6cbbff92c512
omonuj <jonahodoh@gmail.com>
2023-05-20 22:24:13 +0100

    Complete CDK bootstrap and deploy steps

--- cdk/steps.sh
+++ cdk/steps.sh
@@ -22,3 +22,15 @@ npm install @aws-cdk/aws-s3 @aws-cdk/aws-iam @aws-cdk/aws-lambda @aws-cdk/aws-la
 mkdir lambda && touch index.py
 
 # 4. bootstrap the CDK application
+cdk bootstrap
+
+# 5. (optional) synthesize as a CloudFormation template
+cdk synth
+
+
+# 6. deploy the CDK stack
+cdk deploy
+
+# 7. empty the s3 bucket
+# 8. destroy the stack
+cdk destroy
