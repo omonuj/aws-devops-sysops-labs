2391647aa49c87545b15baf79a73a10cd19fe905
omonuj <jonahodoh@gmail.com>
2023-05-29 12:57:50 +0100

    Scaffold managed instance registration setup

---
+++ ssm/managed-instance-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# if you need to install the SSM Agent manually:
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
+# check status ubuntu
