5285ac1209bcc9ccef1d8617a055d404d124663f
omonuj <jonahodoh@gmail.com>
2023-06-15 17:26:06 +0100

    Extend Kinesis Data Streams CLI walkthrough

--- kinesis/kinesis-data-streams.sh
+++ kinesis/kinesis-data-streams.sh
@@ -6,3 +6,10 @@ aws --version
 # PRODUCER
 
 # CLI v2
+aws kinesis put-record --stream-name test --partition-key user1 --data "user signup" --cli-binary-format raw-in-base64-out
+
+# CLI v1
+aws kinesis put-record --stream-name test --partition-key user1 --data "user signup"
+
+
+# CONSUMER 
