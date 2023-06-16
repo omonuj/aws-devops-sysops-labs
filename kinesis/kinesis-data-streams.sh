4590f6a96d74f942ae95de9921268f61dccf733b
omonuj <jonahodoh@gmail.com>
2023-06-16 22:36:14 +0100

    Wrap up Kinesis Data Streams CLI walkthrough

--- kinesis/kinesis-data-streams.sh
+++ kinesis/kinesis-data-streams.sh
@@ -13,3 +13,11 @@ aws kinesis put-record --stream-name test --partition-key user1 --data "user sig
 
 
 # CONSUMER 
+
+# describe the stream
+aws kinesis describe-stream --stream-name test
+
+# Consume some data
+aws kinesis get-shard-iterator --stream-name test --shard-id shardId-000000000000 --shard-iterator-type TRIM_HORIZON
+
+aws kinesis get-records --shard-iterator <>
